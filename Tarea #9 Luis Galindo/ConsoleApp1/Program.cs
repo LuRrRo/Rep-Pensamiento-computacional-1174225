34eabf2be001cd3e55c86142d7359265d5889bf0
LuRrRo <117584853+LuRrRo@users.noreply.github.com>
2025-05-20 20:37:59 -0600

    Add files via upload

---
+++ Tarea #9 Luis Galindo/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Bienvendo! Por favor ingrese la hora del día en formato de 24 horas(solo coloque la hora):");
+int horaDelDía = Convert.ToInt16(Console.ReadLine());
+if (horaDelDía >= 0 && horaDelDía <= 11)
+{ 
+    Console.WriteLine("Buenos dias!");
+}
+else if (horaDelDía >= 12 && horaDelDía <= 18)
+{
+    Console.WriteLine("Buenas Tardes!");
+}
+else if (horaDelDía >= 19 && horaDelDía <= 23)
+{
+    Console.WriteLine("Buenas Noches!");
+}
+else
+{
+    Console.WriteLine("Ingrese una hora apropiada");
+}
---
+++ Tarea#8 Luis Galindo/Tarea #1/Tarea.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Bienvenido \nIngrese su nombre completo por favor:");
+string nombre = Console.ReadLine();
+Console.WriteLine("Ahora, ingresa su altura en metros por favor:");
+double altura = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ahora, ingresa su edad por favor:");
+int edad = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Ahora, ingresa la inicial de tu mascota por favor:");
+char inicial = Convert.ToChar(Console.ReadLine());
+Console.WriteLine("Su nombre es " + nombre + "\nsu edad es " + edad + " años \nSu altura es de " + altura + " metros \nY la inicial de su mascota es "+ inicial);
